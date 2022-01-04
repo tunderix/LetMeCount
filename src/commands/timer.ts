ebbc0d8657e77e68f5eb286ce45552072e841fb4
Pesuankka <51086305+Pesuankka@users.noreply.github.com>
2022-01-04 22:39:46 +0200

    Create timer.ts
    -Created bot timer component

---
+++ src/commands/timer.ts
@@ -0,0 +1,26 @@
+import {
+    Discord,
+    SimpleCommand,
+    SimpleCommandMessage,
+    SimpleCommandOption,
+  } from "discordx";
+  
+  const delay = (ms:number) => new Promise(res => setTimeout(res, ms));
+
+  @Discord()
+  class TimerCommand {
+      
+    @SimpleCommand("timer")
+    
+    async timer( @SimpleCommandOption("seconds", { type: "NUMBER" }) seconds: number | undefined, command: SimpleCommandMessage) {
+      
+      const user = command.message.member?.displayName
+      
+      if (!seconds) return
+      command.message.reply(`Started counting..` + user);
+      await delay(seconds * 1000)
+      command.message.reply(`waited ` + seconds + ` seconds`);
+    }
+  
+  }
+  
