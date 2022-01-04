939c08156b443ca01a4ee117eae8e1fbaf181957
Pesuankka <51086305+Pesuankka@users.noreply.github.com>
2022-01-04 22:42:01 +0200

    Update timer.ts
    -Formated with prettier

--- src/commands/timer.ts
+++ src/commands/timer.ts
@@ -1,26 +1,25 @@
 import {
-    Discord,
-    SimpleCommand,
-    SimpleCommandMessage,
-    SimpleCommandOption,
-  } from "discordx";
-  
-  const delay = (ms:number) => new Promise(res => setTimeout(res, ms));
+  Discord,
+  SimpleCommand,
+  SimpleCommandMessage,
+  SimpleCommandOption,
+} from 'discordx';
 
-  @Discord()
-  class TimerCommand {
-      
-    @SimpleCommand("timer")
-    
-    async timer( @SimpleCommandOption("seconds", { type: "NUMBER" }) seconds: number | undefined, command: SimpleCommandMessage) {
-      
-      const user = command.message.member?.displayName
-      
-      if (!seconds) return
-      command.message.reply(`Started counting..` + user);
-      await delay(seconds * 1000)
-      command.message.reply(`waited ` + seconds + ` seconds`);
-    }
-  
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+@Discord()
+class TimerCommand {
+  @SimpleCommand('timer')
+  async timer(
+    @SimpleCommandOption('seconds', { type: 'NUMBER' })
+    seconds: number | undefined,
+    command: SimpleCommandMessage
+  ) {
+    const user = command.message.member?.displayName;
+
+    if (!seconds) return;
+    command.message.reply(`Started counting..` + user);
+    await delay(seconds * 1000);
+    command.message.reply(`waited ` + seconds + ` seconds`);
   }
-  
+}
